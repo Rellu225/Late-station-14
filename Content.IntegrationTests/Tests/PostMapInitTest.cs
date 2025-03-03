12a6b2da9b68710f8d11d3a563edc2f8ea8524f8
ReboundQ3 <ReboundQ3@gmail.com>
2025-03-03 19:44:43 +0100

    Update PostMapInitTest.cs
    Signed-off-by: ReboundQ3 <ReboundQ3@gmail.com>

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -52,7 +52,7 @@ namespace Content.IntegrationTests.Tests
             "Aspid", // Harmony, playtest for upstream by Golinth
             "Atlas", // Harmony revived by Kravin
             "Mira", // Harmony, developed by tanuko
-            "Torpedo" // LateStation, developed by Rellu
+            "Torpedo", // LateStation, developed by Rellu
             "Dev",
             "TestTeg",
             "Fland",
